7ea4894e21629f0470a119ad1748c63d8a6bebe9
Mehul Goel <mehulgoel873@gmail.com>
2024-06-12 18:03:28 -0700

    passed test cases

--- minWindowSubstring.cpp
+++ minWindowSubstring.cpp
@@ -4,12 +4,14 @@
 #include <vector>
 #include <utility>
 #include <set>
+#include <unordered_set>
 #include <stack>
 #include <unordered_map> 
 using namespace std;
 
 string minWindow(string s, string t) {
     unordered_map<char, int> counts;
+    unordered_set<char> need;
     for (char c : t) {
         if (counts.find(c) == counts.end()) {
             counts[c] = 1;
@@ -17,8 +19,58 @@ string minWindow(string s, string t) {
         else {
             counts[c]++;
         }
+        need.insert(c);
     }
-    return "";
+    
+    int l = 0;
+    int r = 0;
+    unordered_map<char, int> surp;
+    while (!counts.empty()) {
+        if (r >= s.size()) {
+            return "";
+        }
+        if (counts.find(s[r]) != counts.end()) {
+            if (counts[s[r]] == 1) {
+                counts.erase(s[r]);
+            }
+            else counts[s[r]]--;
+        }
+        else if (need.find(s[r]) != need.end()) {
+            surp[s[r]]++;
+        }
+        r++;
+    }
+    
+
+    int len = r - l;
+    while (r <= s.size()) {
+        if (need.find(s[l]) == need.end()) {
+            l++;
+        }
+        else if (surp[s[l]] > 0) {
+            surp[s[l]]--;
+            l++;
+        }
+        else {
+            while (r < s.size() && s[l] != s[r]) {
+                if (need.find(s[r]) != need.end()) {
+                    surp[s[r]]++;
+                }
+                r++;
+            }
+            if (r == s.size()) {
+                return s.substr(l, r - l);;
+            }
+            r++;
+            l++;
+
+        }
+        if (r - l < len) {
+            len = r - l;
+        }
+    }
+
+    return s.substr(l, r - l);
 }
 
 int main() {
